9a63cb7ffcff5bbf9cf8d88af591022203f379a1
Semen Tkachev <tka4off.96@gmail.com>
2022-12-21 12:52:57 +0700

    Update general.py

--- utils/general.py
+++ utils/general.py
@@ -139,7 +139,7 @@ def labels_to_class_weights(labels, nc=80):
         return torch.Tensor()
 
     labels = np.concatenate(labels, 0)  # labels.shape = (866643, 5) for COCO
-    classes = labels[:, 0].astype(np.int)  # labels = [class xywh]
+    classes = labels[:, 0].astype(np.intc)  # labels = [class xywh]
     weights = np.bincount(classes, minlength=nc)  # occurences per class
 
     weights[weights == 0] = 1  # replace empty bins with 1
@@ -151,7 +151,7 @@ def labels_to_class_weights(labels, nc=80):
 def labels_to_image_weights(labels, nc=80, class_weights=np.ones(80)):
     # Produce image weights based on class mAPs
     n = len(labels)
-    class_counts = np.array([np.bincount(labels[i][:, 0].astype(np.int), minlength=nc) for i in range(n)])
+    class_counts = np.array([np.bincount(labels[i][:, 0].astype(np.intc), minlength=nc) for i in range(n)])
     image_weights = (class_weights.reshape(1, nc) * class_counts).sum(1)
     # index = random.choices(range(n), weights=image_weights, k=1)  # weight image sample
     return image_weights
