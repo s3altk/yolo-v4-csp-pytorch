1a1f379cc9d72073b2599f11110831865fc1734b
Semen Tkachev <tka4off.96@gmail.com>
2023-01-29 16:10:41 +0700

    Update export.py

--- export.py
+++ export.py
@@ -10,68 +10,97 @@ from utils.general import check_img_size
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', type=str, default='', help='weights path')
-    parser.add_argument('--img-size', nargs='+', type=int, default=608, help='image size')
-    
-    opt = parser.parse_args()
-    print(opt)
-    
-    # Initialize model with the pretrained weights
-    # Set the model to inference mode
-    model = attempt_load(opt.weights, map_location=torch.device('cpu'))  # load FP32 model
-    model.export = True  # set Detect() layer export=True
+    parser.add_argument('--img-size', type=int, default=608, help='image size')
+    parser.add_argument('--batch-size', type=int, default=1, help='batch size')
+    parser.add_argument('--dynamic', action='store_true', help='dynamic ONNX axes')
+    parser.add_argument('--dynamic-batch', action='store_true', help='dynamic batch onnx for tensorrt and onnx-runtime')
+    parser.add_argument('--grid', action='store_true', help='export Detect() layer grid')
+    parser.add_argument('--topk-all', type=int, default=100, help='topk objects for every images')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='iou threshold for NMS')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='conf threshold for NMS')
+    parser.add_argument('--device', default='cpu', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--simplify', action='store_true', help='simplify onnx model')
 
-    # Input to the model
-    imgsz = check_img_size(opt.img_size, s=model.stride.max())  # check img_size
-    img = torch.randn(1, 3, imgsz, imgsz, requires_grad=True)
-    out = model(img)
-    shape = tuple((out[0] if isinstance(out, tuple) else out).shape)  # model output shape
-    print(f'\nModel with output shape {shape}...')
+    opt = parser.parse_args()
+    opt.dynamic = False if opt.dynamic_batch else opt.dynamic
+    print(opt)
+
+    t = time.time()
+    
+    # Load PyTorch model
+    device = select_device(opt.device)
+    model = attempt_load(opt.weights, map_location=device)  # load FP32 model
+    labels = model.names
+
+    # Checks
+    gs = int(max(model.stride))  # grid size (max stride)
+    opt.img_size = [check_img_size(x, gs) for x in opt.img_size]  # verify img_size are gs-multiples
+
+    # Input
+    img = torch.zeros(opt.batch_size, 3, *opt.img_size).to(device)  # image size
+
+    # Update model
+    model.model[-1].export = not opt.grid  # set Detect() layer grid export
+    y = model(img)  # dry run
 
     # ONNX export
     try:
         import onnx
-        import onnxruntime
 
         print('\nStarting ONNX export with onnx %s...' % onnx.__version__)
         
         f = opt.weights.replace('.pt', '.onnx')  # filename
-        dynamic = {'images': {0: 'batch', 2: 'height', 3: 'width'}}
-        dynamic['output0'] = {0: 'batch', 1: 'anchors'}
+        model.eval()
+
+        output_names = ['classes', 'boxes'] if y is None else ['output']
+        dynamic_axes = None
+
+        if opt.dynamic:
+            dynamic_axes = {'images': {0: 'batch', 2: 'height', 3: 'width'},  # size
+             'output': {0: 'batch', 2: 'y', 3: 'x'}}
+        if opt.dynamic_batch:
+            opt.batch_size = 'batch'
+            dynamic_axes = {
+                'images': {
+                    0: 'batch',
+                }, }
+            output_axes = {
+                    'output': {0: 'batch'},
+                }
+            dynamic_axes.update(output_axes)
+        if opt.grid:
+            model.model[-1].concat = True
         
         warnings.filterwarnings(action='ignore', category=torch.jit.TracerWarning)  # suppress TracerWarning
         
         # Export the model
-        torch.onnx.export(model,                     # model being run
-                  img,                               # model input (or a tuple for multiple inputs)
-                  f,                                 # where to save the model (can be a file or file-like object)
-                  verbose=False,                     # store the trained parameter weights inside the model file
-                  opset_version=12,                  # the ONNX version to export the model to
-                  do_constant_folding=True,          # whether to execute constant folding for optimization
-                  input_names = ['images'],          # the model's input names
-                  output_names = ['output0'],        # the model's output names
-                  dynamic_axes = dynamic)
+        torch.onnx.export(model,
+                  img,
+                  f,
+                  verbose=False,
+                  opset_version=12,
+                  input_names = ['images'],
+                  output_names = output_names,
+                  dynamic_axes = dynamic_axes)
 
         # Checks
         onnx_model = onnx.load(f)  # load onnx model
         onnx.checker.check_model(onnx_model)  # check onnx model
-        print(onnx.helper.printable_graph(onnx_model.graph))  # print a human readable model
+
+        if opt.simplify:
+            try:
+                import onnxsim
+
+                print('\nStarting to simplify ONNX...')
+                onnx_model, check = onnxsim.simplify(onnx_model)
+                assert check, 'assert check failed'
+            except Exception as e:
+                print(f'Simplifier failure: {e}')
         
-        ort_session = onnxruntime.InferenceSession(f)
-        
-        # Compute ONNX Runtime output prediction
-        ort_inputs = {ort_session.get_inputs()[0].name: img.detach().numpy()}
-        ort_outs = ort_session.run(None, ort_inputs)
-        
-        print(f'PyTorch: {out[0].detach().numpy()}')
-        print(f'ONNX: {ort_outs[0]}')
-        
-        # Compare ONNX Runtime and PyTorch results
-        np.testing.assert_allclose(out[0].detach().numpy(), ort_outs[0], rtol=1e-03, atol=1e-05)
-        print('Exported model has been tested with ONNXRuntime and the result looks good!')
-        
+        onnx.save(onnx_model, f)
         print('ONNX export success, saved as %s' % f)
     except Exception as e:
         print('ONNX export failure: %s' % e)
 
     # Finish
-    print('\nExport complete. Visualize with https://github.com/lutzroeder/netron.')
+    print('\nExport complete (%.2fs). Visualize with https://github.com/lutzroeder/netron.' % (time.time() - t))
